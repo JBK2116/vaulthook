b5457e8fd5b7827afbf935460ad82246eeec67d4
jovbk <jovani.badinga7@gmail.com>
2026-03-31 19:03:37 -0400

    Created new types for the frontend to manage logic

---
+++ frontend/src/utils/types.ts
@@ -0,0 +1,27 @@
+export interface WebHookEvent {
+    id: string; // id of the event
+    provider: string; // provider of the event
+    event_type: string; // type of the event
+    received_at: string; // received time of the event (ISO861 Format)
+    response_code: number | null; // response code of the event
+    retry_count: number; // amount of retries for the event
+    forwarded_to: string; // forwared to server address of the event
+    last_error: string | null; // last error that occurred with this event
+    headers: Record<string, string>; // headers sent in event
+    payload: Record<string, unknown>; // payload sent in event
+    delivery_status: DeliveryStatusTypes; // delivery status of the event
+}
+
+export enum DeliveryStatusTypes {
+    Delivered = 'Delivered',
+    Queued = 'Queued',
+    Retrying = 'Retrying',
+    Failed = 'Failed',
+}
+
+export enum DeliveryStatusColors {
+    delivered = 'text-emerald-400',
+    failed = 'text-red-400',
+    retrying = 'text-amber-400',
+    queued = 'text-slate-400',
+}
