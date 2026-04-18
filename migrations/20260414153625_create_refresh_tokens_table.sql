6eaabc77bb1deb0ff9aa9f5d32724729265c9a25
JBK2116 <jovani.badinga7@gmail.com>
2026-04-17 22:29:18 -0400

    Updated the refresh tokens schema, dropped the revoked column as refresh tokens will follow a rotating strategy with no required audit trail

--- migrations/20260414153625_create_refresh_tokens_table.sql
+++ migrations/20260414153625_create_refresh_tokens_table.sql
@@ -3,8 +3,7 @@ CREATE TABLE IF NOT EXISTS refresh_tokens (
     id         UUID         PRIMARY KEY DEFAULT gen_random_uuid(), -- Token ID Generated In Database
     token      VARCHAR(255) UNIQUE NOT NULL, -- Token String 
     expires_at TIMESTAMP    NOT NULL, -- Token Expiry Timestamp
-    created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP, -- Token Created At Timestamp Generated In Database
-    revoked    BOOLEAN      NOT NULL DEFAULT FALSE -- Token Revoked Boolean
+    created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP -- Token Created At Timestamp Generated In Database
 );
 CREATE INDEX idx_refresh_tokens_token ON refresh_tokens(token); -- Index To Speed Up Token Retrievals
 
