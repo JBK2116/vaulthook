fa22b4a84eb60219afaa456b8e17755af6ab6f3b
JBK2116 <jovani.badinga7@gmail.com>
2026-04-25 13:57:54 -0400

    Updated load logic in the providers page.ts

--- frontend/src/routes/(private)/providers/+page.ts
+++ frontend/src/routes/(private)/providers/+page.ts
@@ -1,15 +1,37 @@
 import type { Provider } from '$lib/utils/types';
-import { error } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 
 import type { PageLoad } from './$types';
 
 // Load all providers from the database. Error 500 if unsuccessful.
 export const load: PageLoad = async ({ fetch }) => {
     try {
-        const url = '/api/providers';
-        const res = await fetch(url, { method: 'GET', credentials: 'include' });
+        const res = await fetch('/api/providers', { method: 'GET', credentials: 'include' });
         if (!res.ok) {
-            error(500, 'Internal Server Error');
+            // attempt to refresh the access token
+            if (res.status === 401) {
+                const resTwo = await fetch('/api/refresh', {
+                    method: 'POST',
+                    credentials: 'include',
+                });
+                if (!resTwo.ok) {
+                    redirect(301, '/login');
+                }
+                // try again
+                const resThree = await fetch('/api/providers', {
+                    method: 'GET',
+                    credentials: 'include',
+                });
+                if (!resThree.ok) {
+                    error(500, 'Internal Server Error');
+                }
+
+                const providers: Provider[] = await resThree.json();
+                providers.sort((a, b) => a.name.localeCompare(b.name));
+                return { providers };
+            } else {
+                error(500, 'Internal Server Error');
+            }
         }
         const providers: Provider[] = await res.json();
         providers.sort((a, b) => a.name.localeCompare(b.name));
