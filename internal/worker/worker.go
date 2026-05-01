27b84be10106e77618aa010720163dcda9a5024b
JBK2116 <jovani.badinga7@gmail.com>
2026-05-01 17:46:06 -0400

    Renamed worker file to doc.go
    More files will be added in this pacakge, hence the reasoning.

--- internal/worker/worker.go
+++
@@ -1,5 +0,0 @@
-// Package worker provides background processing for webhook delivery.
-//
-// It handles retry scheduling, event processing, and forwarding
-// independently of the main request lifecycle.
-package worker
