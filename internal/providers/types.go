c19b20ea50236aa7fe3feecb78c134d81daf8775
JBK2116 <jovani.badinga7@gmail.com>
2026-05-02 14:46:48 -0400

    Added descriptive comments into provider types.go

--- internal/providers/types.go
+++ internal/providers/types.go
@@ -37,46 +37,69 @@ type ProviderRouting struct {
 
 // CreateWebhookParams contains only fields required to insert a webhook.
 type CreateWebhookParams struct {
-	ProviderID  uuid.UUID
-	Provider    string
-	EventID     *string
-	EventType   string
-	Headers     json.RawMessage
-	Payload     json.RawMessage
+	// Provider ID
+	ProviderID uuid.UUID
+	// Provider Name
+	Provider string
+	// Webhook Event ID (NULLABLE)
+	EventID *string
+	// Webhook Event Type
+	EventType string
+	// Webhook Event Headers JSON
+	Headers json.RawMessage
+	// Webhook Event Payload JSON
+	Payload json.RawMessage
+	// Webhook Forwarded To Destination
 	ForwardedTo string
-	ReceivedAt  time.Time
+	// Webhook Received At Time
+	ReceivedAt time.Time
 }
 
 // Provider represents a webhook provider.
 type Provider struct {
-	ID             uuid.UUID `json:"id"`
-	Name           string    `json:"name"`
-	SigningSecret  string    `json:"signing_secret"`
-	DestinationURL string    `json:"destination_url"`
-	IsConfigured   bool      `json:"is_configured"`
-	CreatedAt      time.Time `json:"created_at"`
+	ID uuid.UUID `json:"id"`
+	// Provider Name
+	Name string `json:"name"`
+	// Provider Signing Secret For Validating Webhooks
+	SigningSecret string `json:"signing_secret"`
+	// Provider Destination URL To Forward Webhooks
+	DestinationURL string `json:"destination_url"`
+	// Provider Manual Configuration Bool
+	IsConfigured bool `json:"is_configured"`
+	// Provider Created At Time
+	CreatedAt time.Time `json:"created_at"`
 }
 
 // Webhook struct represents a webhook event received by a provider
 type Webhook struct {
-	// IDs
-	ID         uuid.UUID `json:"id"`
+	// Webhook ID
+	ID uuid.UUID `json:"id"`
+	// Webhook Provider ID
 	ProviderID uuid.UUID `json:"provider_id"`
-	Provider   string    `json:"provider"`
-	// Main Information
-	EventID   *string         `json:"event_id"` // NULLABLE
-	EventType string          `json:"event_type"`
-	Headers   json.RawMessage `json:"headers"`
-	Payload   json.RawMessage `json:"payload"`
-	// Event Statistics
+	// Webhook Provider Name
+	Provider string `json:"provider"`
+	// Webhook Event ID (NULLABLE)
+	EventID *string `json:"event_id"`
+	// Webhook Event Type
+	EventType string `json:"event_type"`
+	// Webhook Headers JSON
+	Headers json.RawMessage `json:"headers"`
+	// Webhook Payload JSON
+	Payload json.RawMessage `json:"payload"`
+	// Webhook Delivery Status Enum
 	DeliveryStatus DeliveryStatus `json:"delivery_status"`
-	ForwardedTo    string         `json:"forwarded_to"`
-	ResponseCode   *int           `json:"response_code"`
-	// Event Error Handling
-	RetryCount  int        `json:"retry_count"`
-	NextRetryAt *time.Time `json:"next_retry_at"` // NULLABLE
-	LastError   *string    `json:"last_error"`    // NULLABLE
-	// Event Time Stamps
+	// Webhook Forwarded To URL
+	ForwardedTo string `json:"forwarded_to"`
+	// Webhook Forwarded To Response Code
+	ResponseCode *int `json:"response_code"`
+	// Webhook Retry Count Of Forwarding
+	RetryCount int `json:"retry_count"`
+	// Webhook Next Scheduled Retry At Time (NULLABLE)
+	NextRetryAt *time.Time `json:"next_retry_at"`
+	// Webhook Last Error Message (NULLABLE)
+	LastError *string `json:"last_error"`
+	// Webhook Received At Time
 	ReceivedAt time.Time `json:"received_at"`
-	CreatedAt  time.Time `json:"created_at"`
+	// Webhook Created At Time
+	CreatedAt time.Time `json:"created_at"`
 }
